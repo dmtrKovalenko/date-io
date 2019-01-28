4ff7a1bf84a95c3d322d04ab14e55f009671eee9
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2019-01-28 20:27:19 +0200

    100% cover jalaali utils

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ["**/__tests__/**/*.test.ts"],
+  collectCoverageFrom: ["packages/**/src/**/*"],
   globals: {
     'ts-jest': {
       tsConfig: './__tests__/tsconfig.test.json'
